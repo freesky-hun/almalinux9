ad2ef43023cd89fcdc566bd670544a50fa7328e0
freesky-hun <125161847+freesky-hun@users.noreply.github.com>
2023-10-25 14:51:05 +0200

    Update create_partitions.sh

--- create_partitions.sh
+++ create_partitions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the script is running with root privileges
-if [ "$EUID" -ne 0 ]; then
+if [ "$(id -u)" != "0" ]; then
   echo "This script must be run with root privileges."
   exit 1
 fi
