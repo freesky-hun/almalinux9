e7e9c19de6e0f12b7053b0a859b29f1fdc1a3d94
freesky-hun <125161847+freesky-hun@users.noreply.github.com>
2023-10-25 14:43:42 +0200

    Create create_partitions.sh

---
+++ create_partitions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the script is running with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run with root privileges."
+  exit 1
+fi
+
+# List available drives
+echo "Available drives:"
+lsblk -o NAME,SIZE -d -n
+
+# Prompt the user to select a drive
+read -p "Enter the drive you want to partition (e.g., /dev/sdX): " drive
+
+# Start fdisk for the selected drive
+fdisk "$drive"
+
+echo "Partitioning completed for $drive."
